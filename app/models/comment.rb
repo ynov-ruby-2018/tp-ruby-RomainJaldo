54aa447b1c673a0feb5e6ffc9e8b4eccebdd1c89
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 17:38:15 +0100

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :user # Celui-ci est bon...
 
   has_one :user    # ... Celui-là par contre n'a aucun sens
+  # Manque la validation sur le contenu
 
 end
